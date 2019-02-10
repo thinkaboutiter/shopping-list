6e5157383ed20fab56d951667352eca360a8aa71
boyankov <boyan.yankov@yahoo.com>
2019-02-10 05:51:02 +0200

    attempt to connect to stdout

---
+++ ShoppingList/create-logs-directory.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+LOGS_DIR="/opt/ShoppingList/Sources/ShoppingList/logs"
+
+if [ ! -d ${LOGS_DIR} ]; then
+mkdir ${LOGS_DIR}
+fi
+
+FILE_NAME="application.log"
+if [ ! -f "${LOGS_DIR}/${FILE_NAME}" ]; then
+touch "${LOGS_DIR}/${FILE_NAME}"
+fi
