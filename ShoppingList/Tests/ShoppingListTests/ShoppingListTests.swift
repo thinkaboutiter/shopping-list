83d398692edd44741ab7611a75abbe9cf4a156ae
boyankov <boyan.yankov@yahoo.com>
2019-02-12 01:27:53 +0200

    fix unknown sample testCase

--- ShoppingList/Tests/ShoppingListTests/ShoppingListTests.swift
+++ ShoppingList/Tests/ShoppingListTests/ShoppingListTests.swift
@@ -1,32 +1,12 @@
 import XCTest
-import class Foundation.Bundle
+@testable import ShoppingList
 
 final class ShoppingListTests: XCTestCase {
+    
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-
-        // Some of the APIs that we use below are available in macOS 10.13 and above.
-        guard #available(macOS 10.13, *) else {
-            return
-        }
-
-        let fooBinary = productsDirectory.appendingPathComponent("ShoppingList")
-
-        let process = Process()
-        process.executableURL = fooBinary
-
-        let pipe = Pipe()
-        process.standardOutput = pipe
-
-        try process.run()
-        process.waitUntilExit()
-
-        let data = pipe.fileHandleForReading.readDataToEndOfFile()
-        let output = String(data: data, encoding: .utf8)
-
-        XCTAssertEqual(output, "Hello, world!\n")
+        // sample test
+        let result = 0
+        XCTAssertEqual(result, 0, "Ooops!")
     }
 
     /// Returns path to the built products directory.
