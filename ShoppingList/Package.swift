8fc5c29a31beee4d85d43163cbb9b1011e1cb4b5
boyankov <boyan.yankov@yahoo.com>
2019-02-12 01:07:46 +0200

    updated packages

--- ShoppingList/Package.swift
+++ ShoppingList/Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "ShoppingList",
+    products: [
+        .executable(
+            name: "ShoppingList",
+            targets: ["ShoppingList"]
+        ),
+    ],
     dependencies: [
         .package(url: "https://github.com/SwiftORM/MongoDB-StORM.git", from: "3.0.0"),
         .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
@@ -19,5 +25,11 @@ let package = Package(
                 "SimpleLogger"
             ],
             path: "Sources"),
+        .testTarget(
+            name: "ShoppingListTests",
+            dependencies: [
+                "ShoppingList",
+                ],
+            path: "Tests")
     ]
 )
