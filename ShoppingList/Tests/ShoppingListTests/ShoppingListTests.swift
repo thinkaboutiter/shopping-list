261ebaed3dc900003e93cb17f09ad366014f5a7f
boyankov <boyan.yankov@yahoo.com>
2019-02-12 02:25:22 +0200

    removed un used test variable

--- ShoppingList/Tests/ShoppingListTests/ShoppingListTests.swift
+++ ShoppingList/Tests/ShoppingListTests/ShoppingListTests.swift
@@ -8,18 +8,6 @@ final class ShoppingListTests: XCTestCase {
         XCTAssertEqual(0, 0)
     }
 
-    /// Returns path to the built products directory.
-    var productsDirectory: URL {
-      #if os(macOS)
-        for bundle in Bundle.allBundles where bundle.bundlePath.hasSuffix(".xctest") {
-            return bundle.bundleURL.deletingLastPathComponent()
-        }
-        fatalError("couldn't find the products directory")
-      #else
-        return Bundle.main.bundleURL
-      #endif
-    }
-
     static var allTests = [
         ("testExample", testExample),
     ]
